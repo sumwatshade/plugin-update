8638fbd771842317e5d6b8a73af604bd691d0de4
lshadler <lshadler13@gmail.com>
2021-06-04 09:35:57 -0700

    fix: configuration changes

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "oclif",
+    "oclif-typescript",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  rules: {
+    "prettier/prettier": "error",
+    "no-useless-constructor": "off",
+    "@typescript-eslint/no-useless-constructor": "error",
+  },
+};
